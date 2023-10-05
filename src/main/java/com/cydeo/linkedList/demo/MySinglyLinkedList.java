eb88b278fe3205396c44a83135388d5242c8468a
ziya <koltikin210@gmail.com>
2023-10-05 22:50:05 +0300

    print method created

--- src/main/java/com/cydeo/linkedList/demo/MySinglyLinkedList.java
+++ src/main/java/com/cydeo/linkedList/demo/MySinglyLinkedList.java
@@ -8,4 +8,29 @@ public class MySinglyLinkedList {
     boolean isEmpty(){
         return head == null;
     }
+    void add(int data){
+        Node node = new Node(data);
+        if (isEmpty()){
+            head = tail = node;
+            size ++;
+        }else {
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+
+    }
+
+    void printNode(){
+        Node current = head;
+        while (current != null){
+            if (current.next == null) System.out.println(current.id + "=>null");
+            else {
+                System.out.print(current.id + "=> ");
+
+            }
+            current = current.next;
+
+        }
+    }
 }
--- src/main/java/com/cydeo/linkedList/demo/SinglyLinkedListApp.java
+++ src/main/java/com/cydeo/linkedList/demo/SinglyLinkedListApp.java
@@ -5,5 +5,13 @@ public class SinglyLinkedListApp {
     public static void main(String[] args) {
         MySinglyLinkedList myList = new MySinglyLinkedList();
         System.out.println(myList.isEmpty());
+
+        for (int i = 0; i <10 ; i++) {
+            myList.add(i);
+
+        }
+        myList.printNode();
+
+
     }
 }
