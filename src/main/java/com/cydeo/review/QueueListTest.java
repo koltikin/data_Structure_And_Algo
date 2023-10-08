46fbc4d1e6fe94e2f154597753a13883890373cf
ziya <koltikin210@gmail.com>
2023-10-08 16:10:34 +0300

    custom queue list generated

---
+++ src/main/java/com/cydeo/review/QueueListTest.java
@@ -0,0 +1,17 @@
+package com.cydeo.review;
+
+import java.util.ArrayList;
+
+public class QueueListTest {
+    public static void main(String[] args) {
+        QueueWithList<Integer> myQueue = new QueueWithList<>(new ArrayList<>());
+
+        myQueue.enQueue(1);
+        myQueue.enQueue(2);
+        myQueue.enQueue(3);
+        myQueue.enQueue(4);
+        myQueue.enQueue(5);
+
+        System.out.println(myQueue.deQueue());
+    }
+}
--- src/main/java/com/cydeo/review/QueueWithList.java
+++ src/main/java/com/cydeo/review/QueueWithList.java
@@ -1,4 +1,20 @@
 package com.cydeo.review;
 
-public class QueueWithList {
+import java.util.List;
+
+public class QueueWithList<E> {
+    List<E> list;
+
+    public QueueWithList(List<E> list) {
+        this.list = list;
+    }
+    public void enQueue(E e){
+        list.add(e);
+    }
+
+    public E deQueue(){
+        E result = list.get(0);
+        list.remove(0);
+        return result;
+    }
 }
