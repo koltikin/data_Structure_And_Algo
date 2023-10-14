216da402a975ad13a5cc7f5d907b5b615e9481d8
ziya <koltikin210@gmail.com>
2023-10-14 19:39:12 +0300

    SunsetViews assignment one more solution added

--- src/main/java/com/cydeo/stack/assignment/SunsetViews.java
+++ src/main/java/com/cydeo/stack/assignment/SunsetViews.java
@@ -8,7 +8,7 @@ import java.util.Stack;
 public class SunsetViews {
     public static void main(String[] args) {
         int[] buildings = {3, 5, 4, 1, 3,2};
-        sunsetViews(buildings,"west").forEach(System.out::println);
+        sunsetViews1(buildings,"west").forEach(System.out::println);
     }
 
     private static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
@@ -28,6 +28,27 @@ public class SunsetViews {
 
         }
         if (direction.equalsIgnoreCase("west")) Collections.reverse(stack);
+
+        return new ArrayList<>(stack);
+    }
+
+    private static ArrayList<Integer> sunsetViews1(int[] buildings, String direction) {
+        Stack<Integer> stack = new Stack<>();
+        int i = buildings.length-1;
+        int step = -1;
+        if (direction.equalsIgnoreCase("west")){
+            i = 0;
+            step = 1;
+        }
+        while (i>=0 && i< buildings.length) {
+            if (!stack.isEmpty() && buildings[i] < buildings[stack.peek()]){
+                i += step;
+                continue;
+            }
+            stack.push(i);
+            i += step;
+        }
+        if (direction.equalsIgnoreCase("east"))Collections.reverse(stack);
 
         return new ArrayList<>(stack);
     }
